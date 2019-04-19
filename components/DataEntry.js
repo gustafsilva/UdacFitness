19d235a2a610e497e7cb8122d1b5cc505ed624c0
Gustavo F. Silva <gustafsilva.dev@gmail.com>
2019-04-19 10:23:16 -0300

    Adding states to DataEntry component.

--- components/DataEntry.js
+++ components/DataEntry.js
@@ -5,6 +5,40 @@ import { getMetricMetaInfo } from '../utils/helpers';
 
 
 class DataEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  }
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric);
+
+    this.setState((currentState) => {
+      const count = currentState[metric] + step;
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count,
+      }
+    })
+  }
+
+  decrement = (metric) => {
+    const { step } = getMetricMetaInfo(metric);
+
+    this.setState(() => {
+      const count = state[metric] - step;
+      return {
+        [metric]: count <= 0 ? 0 : count,
+      }
+    })
+  }
+
+  slider = (metric, value) => { this.setState({ [metric]: value }) }
+
 
   render() {
     return (
