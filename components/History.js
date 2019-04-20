b7399b0e3c823c7ea8a92d225e34a2ceb2493b04
Gustavo F. Silva <gustafsilva.dev@gmail.com>
2019-04-20 17:46:53 -0300

    Adding styles in History component.

--- components/History.js
+++ components/History.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import UdaciFitnessCalendar from 'udacifitness-calendar';
 
+import DateHeader from './DateHeader';
 import { fetchCalendarResults } from '../utils/api';
-import { timeToString, getDailyReminderValue } from '../utils/helpers';
+import { timeToString, getDailyReminderValue, getPlatformOS } from '../utils/helpers';
 import { addEntry, receiveEntries } from '../store/actions';
+import { white } from '../utils/colors';
 
 class History extends Component {
   componentDidMount() {
@@ -26,15 +33,25 @@ class History extends Component {
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
     <View>
       {today
-        ? <Text>{JSON.stringify(today)}</Text>
-        : <Text>{JSON.stringify(metrics)}</Text>
+        ? (
+          <View style={styles.item}>
+            <DateHeader date={formattedDate} />
+            <Text style={styles.noDataText}>{today}</Text>
+          </View>
+        )
+        : (
+          <TouchableOpacity onPress={() => {}}>
+            <Text>{JSON.stringify(metrics)}</Text>
+          </TouchableOpacity>
+        )
       }
     </View>
   )
 
-  renderEmptyDate = (formattedDate) => (
-    <View>
-      <Text>No data for this day.</Text>
+  renderEmptyDate = formattedDate => (
+    <View style={styles.item}>
+      <DateHeader date={formattedDate} />
+      <Text style={styles.noDataText}>No data for this day.</Text>
     </View>
   )
 
@@ -51,6 +68,30 @@ class History extends Component {
   }
 }
 
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: getPlatformOS() === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+});
+
 History.propTypes = {
   dispatch: PropTypes.func.isRequired,
 };
@@ -58,5 +99,6 @@ History.propTypes = {
 const mapStateToProps = entries => ({
   entries,
 });
+
 
 export default connect(mapStateToProps)(History);
