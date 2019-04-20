b2240e4939579e8ba9194ad080a5c8b28dbd6cef
Gustavo F. Silva <gustafsilva.dev@gmail.com>
2019-04-20 16:16:55 -0300

    Adding styles in TextButton component.

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 
-const TextButton = ({ children, onPress }) => (
+import { purple } from '../utils/colors';
+
+const TextButton = ({ children, onPress, style }) => (
   <TouchableOpacity onPress={onPress}>
-    <Text>{children}</Text>
+    <Text style={[styles.reset, style]}>{children}</Text>
   </TouchableOpacity>
 );
+
+const styles = StyleSheet.create({
+  reset: {
+    textAlign: 'center',
+    color: purple,
+  }
+});
+
+TextButton.defaultProps = {
+  style: {},
+};
 
 TextButton.propTypes = {
   children: PropTypes.string.isRequired,
   onPress: PropTypes.func.isRequired,
+  style: PropTypes.objectOf(PropTypes.any),
 };
 
 export default TextButton;
