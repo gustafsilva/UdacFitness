bbfc2d40e19e12097a55b26e9a657f29004fca74
Gustavo F. Silva <gustafsilva.dev@gmail.com>
2019-04-20 16:14:35 -0300

    Adding style in DateHadear component.

--- components/DateHeader.js
+++ components/DateHeader.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text } from 'react-native';
+import { purple } from '../utils/colors';
 
 const DataHeader = ({ date }) => (
   <View>
-    <Text>{date}</Text>
+    <Text style={{ color: purple, fontSize: 25 }}>{date}</Text>
   </View>
 );
 
