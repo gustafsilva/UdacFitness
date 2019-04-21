ecdc330ab67bd3a25c672de95ce6cd58b924b1e5
Gustavo F. Silva <gustafsilva.dev@gmail.com>
2019-04-21 10:21:12 -0300

    Configurating title navigation in EntryDetail component.

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text } from 'react-native';
 
-const EntryDetail = ({ navigation }) => <Text>{`Entry Detail - ${JSON.stringify(navigation.state.params.entryId)}`}</Text>;
+class EntryDetail extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params;
+
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+
+    return { title: `${month}/${day}/${year}` };
+  }
+
+  render() {
+    const { navigation } = this.props;
+
+    return <Text>{`Entry Detail - ${JSON.stringify(navigation.state.params.entryId)}`}</Text>;
+  }
+}
 
 EntryDetail.propTypes = {
   navigation: PropTypes.objectOf(PropTypes.any).isRequired,
